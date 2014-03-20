6249781c17a65e1f2372835b1a1db8a92a035880
zhangkai123 <176776005@qq.com>
2014-03-20 17:53:58 +0800

    add the message data model

---
+++ Tuijian/Tuijian/TJMessage.h
@@ -0,0 +1,16 @@
+//
+//  TJMessage.h
+//  Tuijian
+//
+//  Created by zhang kai on 3/20/14.
+//  Copyright (c) 2014 zhang kai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TJMessage : NSObject
+
+@property(nonatomic,strong) NSString *imageUrl;
+@property(nonatomic,strong) NSString *messageTitle;
+@property(nonatomic,strong) NSString *message;
+@end
---
+++ Tuijian/Tuijian/TJMessage.m
@@ -0,0 +1,14 @@
+//
+//  TJMessage.m
+//  Tuijian
+//
+//  Created by zhang kai on 3/20/14.
+//  Copyright (c) 2014 zhang kai. All rights reserved.
+//
+
+#import "TJMessage.h"
+
+@implementation TJMessage
+@synthesize imageUrl ,messageTitle ,message;
+
+@end
