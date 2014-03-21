03b0fa1b1046b287159700d838fde09ed631bfc7
zhangkai123 <176776005@qq.com>
2014-03-21 16:48:13 +0800

    new post view controller

--- Tuijian/Tuijian/TJPostViewController.m
+++ Tuijian/Tuijian/TJPostViewController.m
@@ -8,8 +8,12 @@
 
 #import "TJPostViewController.h"
 
-@interface TJPostViewController ()
-
+@interface TJPostViewController ()<UITextFieldDelegate,UITextViewDelegate>
+{
+    UITextField *titleTextField;
+    UITextView *tuijianTextView;
+    UILabel *placeHolderLabel;
+}
 @end
 
 @implementation TJPostViewController
@@ -41,15 +45,63 @@
     UIImageView *imageView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 64 + 10, 50, 50)];
     imageView.image = self.cropedImage;
     [self.view addSubview:imageView];
+    
+    titleTextField = [[UITextField alloc]initWithFrame:CGRectMake(70, 64 + 10 + 15, 240, 20)];
+    titleTextField.backgroundColor = [UIColor whiteColor];
+    titleTextField.placeholder = @"标题";
+    titleTextField.delegate = self;
+    [titleTextField becomeFirstResponder];
+    [self.view addSubview:titleTextField];
+    
+    tuijianTextView = [[UITextView alloc]initWithFrame:CGRectMake(10, 64 + 10 + 50 + 10, 300, 160)];
+//    tuijianTextView.backgroundColor = [UIColor blueColor];
+//    [tuijianTextView setBackgroundColor:[UIColor colorWithWhite:1.0 alpha:0.8]];
+    tuijianTextView.font = [UIFont systemFontOfSize:15];
+    tuijianTextView.autocorrectionType = UITextAutocorrectionTypeNo;
+    tuijianTextView.keyboardType = UIKeyboardTypeDefault;
+    tuijianTextView.returnKeyType = UIReturnKeyDone;
+    tuijianTextView.textAlignment = NSTextAlignmentLeft;
+    tuijianTextView.scrollEnabled = NO;
+    tuijianTextView.delegate = self;
+    [self.view addSubview:tuijianTextView];
+    
+    placeHolderLabel = [[UILabel alloc]initWithFrame:CGRectMake(0.0, 0.0,tuijianTextView.frame.size.width - 10.0, 34.0)];
+    [placeHolderLabel setText:@"推荐理由"];
+    placeHolderLabel.textAlignment = NSTextAlignmentLeft;
+    [placeHolderLabel setBackgroundColor:[UIColor clearColor]];
+    [placeHolderLabel setTextColor:[UIColor lightGrayColor]];
+    [tuijianTextView addSubview:placeHolderLabel];
 }
 -(void)cancelPost
 {
-    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 -(void)postItem
 {
-    
+    NSString *tuijianText = tuijianTextView.text;
+    [[TJDataController sharedDataController]saveItem:tuijianText uploadImage:self.cropedImage success:^(id Json){
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }failure:^(NSError *error){
+        
+    }];
 }
+#pragma text view delegate
+- (BOOL)textViewShouldBeginEditing:(UITextView *)textView{
+    return YES;
+}
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    placeHolderLabel.hidden = YES;
+}
+- (BOOL)textViewShouldEndEditing:(UITextView *)textView{
+    return YES;
+}
+- (void)textViewDidEndEditing:(UITextView *)textView{
+    if (textView.text.length == 0) {
+        placeHolderLabel.hidden = NO;
+    }
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
