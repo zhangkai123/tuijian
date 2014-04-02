03f92a1e3a4d02da93b0a50cc9a2d912384c08c4
zhangkai123 <176776005@qq.com>
2014-04-02 18:02:22 +0800

    change some place holder

--- Tuijian/Tuijian/TJInfoViewController.m
+++ Tuijian/Tuijian/TJInfoViewController.m
@@ -87,7 +87,7 @@
     NSURLRequest *urlRequest = [NSURLRequest requestWithURL:imageUrl];
     __block UIImageView *weakImageView = cell.theImageView;
     [cell.theImageView setImageWithURLRequest:urlRequest
-                                placeholderImage:[UIImage imageNamed:@"placeholder.png"]
+                                placeholderImage:[UIImage imageNamed:@"message_placeholder.png"]
                                          success:^(NSURLRequest *request ,NSHTTPURLResponse *response ,UIImage *image){
                                              
                                              float radius = MAX(image.size.width, image.size.height);
